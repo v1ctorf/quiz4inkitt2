dbdc1ff1f3372acfd44af23e54fe1d3a62e8cec5
Victor F <mail@victorf.ch>
2017-10-02 05:38:15 +0000

    distro ok

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,17 @@
 class AnswersController < ApplicationController
     def index
-      @answers = Answer.all
+      finished = Answer.group(:ip).having('COUNT(*) == 5').count
+      @answers = Answer.where('ip IN (?)',  finished.keys)
+
+      @distribution = []
+      @questions = []
+      
+      for i in 0..4
+         @distribution[i] = @answers.where("question == ?",i + 1).group(:option).count
+         @questions[i] = generate_question(i)
+      end
+      
+      
     end
     
     def new
@@ -48,7 +59,6 @@ class AnswersController < ApplicationController
     def destroy
       @answer = Answer.find(params[:id])
       @answer.destroy
-   
       redirect_to answers_path
     end
     
@@ -59,13 +69,12 @@ class AnswersController < ApplicationController
     
     def answered_questions
       Answer.where(["ip = ?", request.remote_ip]).length
-    
     end
     
     def generate_question(question)
       questions = [
         {
-          'question' => "How many books did you read last year?",
+          'question' => "1) How many books did you read last year?",
           'options' => [
             'none',
             'less than 3',
@@ -73,13 +82,13 @@ class AnswersController < ApplicationController
             "10 or more"
           ]
         },{
-          'question' => "What book category do you prefere?",
+          'question' => "2) What book category do you prefere?",
           'options' => [
             'fiction',
             'non-fiction'
           ]
         },{
-          'question' => "What's your favorite way to read a book?",
+          'question' => "3) What's your favorite way to read a book?",
           'options' => [
             'paper book',
             'tablet or mobile phone',
@@ -87,7 +96,7 @@ class AnswersController < ApplicationController
             'I prefer audio books'
           ]
         },{
-          'question' => "Usually, how do you choose a book?",
+          'question' => "4) Usually, how do you choose a book?",
           'options' => [
             "friends' recomendations",
             'best sellers',
@@ -96,7 +105,7 @@ class AnswersController < ApplicationController
             "by myself"
           ]
         },{
-          'question' => 'How much do you usually pay for a book?',
+          'question' => '5) How much do you usually pay for a book?',
           'options' => [
             "I don't use to pay for books",
             "less than 5 euros",
